936bb518d2be9562d6ff5a936fbb6b623975a213
unknown <nishant.meher.9595@gmail.com>
2022-06-08 09:49:06 +0900

    Routing Pages

---
+++ next-routing/pages/about.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css'
+import React from 'react'
+
+function About() {
+  return (
+    <div className={styles.container}>
+        <h1 >About</h1>
+    </div>
+  )
+}
+
+export default About
---
+++ next-routing/pages/profile.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css'
+import React from 'react'
+
+function Profile() {
+  return (
+    <div className={styles.container}>
+        <h1>Profile</h1>
+    </div>
+  )
+}
+
+export default Profile
