15db32242f7dfff1625298230b3f593c53536a40
unknown <nishant.meher.9595@gmail.com>
2022-06-08 14:49:56 +0900

    Static Data

---
+++ Rendering-Difference/next-demo/pages/users.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function UserList({ users }) {
+  return (
+    <div>
+        <>
+        <h1>List of Users</h1>
+        
+            {users.map(user => {
+                return (
+                    <div key={user.id}>
+                        <p>{user.name}</p>
+                        <p>{user.email}</p>
+                    </div>
+                )
+            })
+        }
+        </>
+    </div>
+  )
+}
+
+export default UserList
+
+export async function getStaticProps() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    console.log(data)
+
+    return {
+        props : {
+            users:data,
+        },
+    }
+}
