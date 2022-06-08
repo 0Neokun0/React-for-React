63406ace47e74febac181ecfa0cb8ff49abd1b82
unknown <nishant.meher.9595@gmail.com>
2022-06-08 11:21:59 +0900

    Link Component Navigation

--- next-routing/pages/index.js
+++ next-routing/pages/index.js
@@ -1,11 +1,22 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Home Page</h1>
+      <Link href = '/blog'>
+      <a>
+        Blog
+      </a>
+      </Link>
+      <Link href = '/product'>
+      <a>
+        Product
+      </a>
+      </Link>
     </div>
   )
 }
--- next-routing/pages/product/index.js
+++ next-routing/pages/product/index.js
@@ -1,13 +1,40 @@
 import styles from '../../styles/Home.module.css'
+import Link from 'next/link'
 import React from 'react'
 
-function ProductList() {
+function ProductList( { productId = 100}) {
   return (
+    <>
+    <Link href='/'>
+    <h1 className={styles.container}>
+      Home
+    </h1>
+    </Link>
     <div className={styles.container}>
-        <h2>Product List 1 </h2>
-        <h2>Product List 2 </h2>
-        <h2>Product List 3 </h2>
+        <h2>
+          <Link href = '/product/1'>
+          <a>
+            Product 1
+          </a>
+          </Link>
+        </h2>
+        <h2><Link href = '/product/2'>
+          <a>
+            Product 2
+          </a>
+          </Link></h2>
+        <h2><Link href = '/product/3' replace>
+          <a>
+            Product 3
+          </a>
+          </Link></h2>
+         <h2><Link href = {`/product/${productId}`}>
+          <a>
+            Product {productId}
+          </a>
+          </Link></h2>
     </div>
+    </>
   )
 }
 
