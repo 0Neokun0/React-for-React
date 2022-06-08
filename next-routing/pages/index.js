ddee52158a6b8456c6f4a2d6ec7d8f7a4d71c1fd
unknown <nishant.meher.9595@gmail.com>
2022-06-08 11:46:23 +0900

    Navigating Programmatically

--- next-routing/pages/index.js
+++ next-routing/pages/index.js
@@ -1,9 +1,19 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  const router = useRouter ()
+
+
+  const handleClick = () => {
+    console.log('Placing Your Order')
+    router.push('/product')
+  }
+
   return (
     <div className={styles.container}>
       <h1>Home Page</h1>
@@ -17,6 +27,9 @@ export default function Home() {
         Product
       </a>
       </Link>
+      <button onClick={handleClick}>
+        Place Order
+      </button>
     </div>
   )
 }
