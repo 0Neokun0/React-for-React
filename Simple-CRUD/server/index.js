8a193484c0d83e4f22fbb92849a343eaf7427011
Nishant Meher <90218986+0Neokun0@users.noreply.github.com>
2022-06-16 08:03:43 +0900

    Updated

--- Simple-CRUD/server/index.js
+++ Simple-CRUD/server/index.js
@@ -1,7 +1,10 @@
 const express = require ( 'express' );
 const app = express();
+const mysql = require('mysql');
+const cors = require('cors')
 
-const mysql = require('mysql')
+app.use(cors());
+
 
 const db = mysql.createConnection({
     user: 'root',
