b9f971c9db2f382d03fdd76dbffe7b4e962e8896
Nishant Meher <90218986+0Neokun0@users.noreply.github.com>
2022-06-19 20:27:19 +0900

    Modal Popup

---
+++ modal-popup/src/App.js
@@ -0,0 +1,10 @@
+import Modal from "./Components/Modal";
+const App = () => {
+  return (
+    <div className="App h-screen flex flex-col items-center justify-center bg-purple-200">
+      <Modal />
+    </div>
+  );
+};
+
+export default App;
